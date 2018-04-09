f745c3e803283f719c5219e2e8125e9f8e7bcada
Ben Alkov <ben.alkov@gmail.com>
2018-04-08 20:16:42 -0400

    tweaks

--- scratchpad.py
+++ scratchpad.py
@@ -1,4 +1,4 @@
-from pyo import *
+from pyo import *  # Implicitly imports `random`?
 
 from ground_state.pyo.utils import Tempo
 from ground_state.pyo.utils import dbToAmp
@@ -10,64 +10,92 @@ from ground_state.pyo.utils import serverSetup
 # Windows
 s = serverSetup(10, 700)
 s.boot()
-s.start()
-s.setAmp(dbToAmp(-40))
+s.setAmp(dbToAmp(-34))
 
-shruti = [261.6256,
-          275.6220,
-          279.0673,
-          290.6951,
-          294.3288,
-          310.0747,
-          313.9507,
-          327.0319,
-          331.1198,
-          348.8341,
-          353.1945,
-          367.9109,
-          372.5098,
-          392.4383,
-          413.4330,
-          418.6009,
-          436.0426,
-          441.4931,
-          465.1121,
-          470.9260,
-          490.5479,
-          496.6798,
-          523.251]
+root = 261.6256
+shruti_ratios = [1.0,
+                 256.0 / 243.0,
+                 16.0 / 15.0,
+                 10.0 / 9.0,
+                 9.0 / 8.0,
+                 32.0 / 27.0,
+                 6.0 / 5.0,
+                 5.0 / 4.0,
+                 81.0 / 64.0,
+                 4.0 / 3.0,
+                 27.0 / 20.0,
+                 45.0 / 32.0,
+                 729.0 / 512.0,
+                 3.0 / 2.0,
+                 128.0 / 81.0,
+                 8.0 / 5.0,
+                 5.0 / 3.0,
+                 27.0 / 16.0,
+                 16.0 / 9.0,
+                 9.0 / 5.0,
+                 15.0 / 8.0,
+                 243.0 / 128.0,
+                 2.0]
 
+shruti_high = [root * x for x in shruti_ratios]
+shruti_ratios_low = shruti_ratios[:]
+shruti_ratios_low.reverse()
+shruti_low = [root / x for x in shruti_ratios_low]
+shruti = shruti_low[:-1] + shruti_high
 
-# met = Metro(.125).play()
-# choice = TrigXnoise(met, dist='loopseg', x1=0.5, x2=0.135)  # Outputs (0,1)
-# noteIndex = int(rescale(choice.get(), 0.0, 1.0, 0.0, 21.0))
-# out = SineLoop([shruti[noteIndex], shruti[noteIndex]], feedback=0.07).out()
-# out.setFeedback(.03)
+lead_note_index = 22
+accomp_note_index = 22
+lead_met = Metro(0.125)
+accomp_met = Metro(2)
+drone = SineLoop([shruti[0], shruti[0]], feedback=0.07).out()
+lead = SineLoop([shruti[lead_note_index], shruti[lead_note_index]], feedback=0.12, mul=dbToAmp(-4))
+accomp = SineLoop([shruti[accomp_note_index], shruti[accomp_note_index]], feedback=0.08, mul=dbToAmp(-8))
+mm = Mixer(outs=1, chnls=2, time=.025)
+mm.addInput(0, lead)
+mm.addInput(1, accomp)
+mm.setAmp(0, 0, 0.5)
+mm.setAmp(1, 0, 0.5)
 
-
-met = Metro(.125).play()
-out = SineLoop([shruti[note_index], shruti[note_index]], feedback=0.07).out()
-
-note_index = 0
-def chooseStep():
-    global note_index
+def choose_lead_step():
+    global lead_note_index
+    step = int(random.normalvariate(1, 3))
+    lead_note_index += step
+    if lead_note_index < 0:
+        lead_note_index *= -1
+    if lead_note_index > len(shruti) - 1:
+        lead_note_index = (len(shruti) - 1) - lead_note_index
     try:
-        step = int(random.normalvariate(1, 0.75))
-        if note_index == 21 or random.uniform(0, 1) < 0.5:
-            step = step * -1
-        note_index += step
-        out.freq = [shruti[note_index], shruti[note_index]]
+        lead.freq = [shruti[lead_note_index], shruti[lead_note_index]]
     except:
-        print('note_index is {0}, step is {1}'.format(note_index, step))
+        print('lead_note_index is {0}; step is {1}'.format(lead_note_index, step))
         raise
 
-tf = TrigFunc(met, chooseStep)
+def choose_accomp_step():
+    global accomp_note_index
+    step = int(random.normalvariate(1, 3))
+    accomp_note_index += step
+    if accomp_note_index < 0:
+        accomp_note_index *= -1
+    if accomp_note_index > len(shruti) - 1:
+        accomp_note_index = (len(shruti) - 1) - accomp_note_index
+    try:
+        accomp.freq = [shruti[accomp_note_index], shruti[accomp_note_index]]
+    except:
+        print('accomp_note_index is {0}; step is {1}'.format(accomp_note_index, step))
+        raise
+
+
+d = Delay(mm, delay=0.2, feedback=0.5, mul=0.4).out()
+
+lead_trig = TrigFunc(lead_met, choose_lead_step)
+accomp_trig = TrigFunc(accomp_met, choose_accomp_step)
+lead_met.play()
+accomp_met.play()
+
 
 s.start()
 s.setAmp(dbToAmp(-40))
 
-Clean_objects(1, met, out, note_index, tf).start()
-Clean_objects(1, s)
 s.stop()
 s.shutdown()
 s.reinit()
